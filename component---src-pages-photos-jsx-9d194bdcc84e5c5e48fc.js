81c86824faf2d2601efb728b626c1622527d22b2
Orestis Ioannou <o.ioannou@sysdream.com>
2018-09-01 01:19:19 +0200

    Updates

---
+++ component---src-pages-photos-jsx-9d194bdcc84e5c5e48fc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{211:function(e,a,t){"use strict";t.r(a),t.d(a,"pageQuery",function(){return d});var n=t(1),r=t.n(n),o=t(0),p=t.n(o),s=t(237),c=t(239),u=t(241),i=t(320);function l(e){var a=e.data.allMarkdownRemark.edges;return r.a.createElement(c.a,{title:"Photography articles"},r.a.createElement(u.a,null,r.a.createElement(i.a,{posts:a})))}l.propTypes={data:p.a.shape().isRequired};var d="3345844283";a.default=Object(s.a)(l)}}]);
+//# sourceMappingURL=component---src-pages-photos-jsx-9d194bdcc84e5c5e48fc.js.map
