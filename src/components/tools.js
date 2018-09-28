2dda66cc05b19e2b19592b139e2825c308d0f71c
Orestis Ioannou <oorestisime@gmail.com>
2018-09-29 01:42:28 +0200

    Filter out posts without coordinates before normalizing them

--- src/components/tools.js
+++ src/components/tools.js
@@ -2,7 +2,9 @@ import _ from 'lodash';
 
 const capitalize = text => text.charAt(0).toUpperCase() + text.slice(1);
 const coordinates = posts => _.uniqBy(
-  _.flatten(posts.map(post => post.frontmatter.coordinates.map(coord => coord))),
+  _.flatten(posts
+    .filter(post => post.frontmatter.coordinates && post.frontmatter.type !== 'friends')
+    .map(post => post.frontmatter.coordinates.map(coord => coord))),
   v => `${v.coordinates[0]} ${v.coordinates[1]}`,
 );
 const groupBy = (posts, field) => _.groupBy(posts, post => post.frontmatter[field]);
